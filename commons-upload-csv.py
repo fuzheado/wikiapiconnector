60f4c41c106fa9a4152964002fdc856fffc58897
User:Fuzheado <andrew.lih@gmail.com>
2023-12-13 07:27:52 +0000

    commons upload working solidly from CSV

--- commons-upload-csv.py
+++ commons-upload-csv.py
@@ -5,13 +5,13 @@
 # Example row:
 # nmnhbotany_2546215,https://ids.si.edu/ids/download?id=NMNH-00651834.jpg,Etlingera sp._nmnhbotany_2546215_NMNH-00651834.jpg,Uploaded by Wiki API Connector,"{{Information... }}"
 
+import os
 import sys
 import requests
 import hashlib
 import pywikibot
 import csv
 import argparse
-from io import BytesIO
 from typing import Optional, List, Tuple
 from pywikibot.specialbots import UploadRobot
 
@@ -49,34 +49,33 @@ def get_final_url(url: str, max_redirects: int = 10, current_redirects: int = 0)
         return None
 
 
-def download_image(url: str) -> Optional[BytesIO]:
-    """Download the image from the URL and return it as a BytesIO object."""
+def download_image(url: str, filename: str) -> Optional[str]:
+    """Download the image from the URL and save it as the given filename."""
     try:
         response = requests.get(url, allow_redirects=True, timeout=10)
         response.raise_for_status()
-        return BytesIO(response.content)
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        return filename
     except requests.RequestException as e:
         print(f"Error occurred while downloading the image: {e}")
         return None
 
-def file_exists_on_commons(image_data: BytesIO, filename: str) -> bool:
+def file_exists_on_commons(filename: str) -> bool:
     """Check if the file already exists on Wikimedia Commons using SHA1 hash."""
     site = pywikibot.Site('commons', 'commons')
-    sha1_hash = hashlib.sha1(image_data.getvalue()).hexdigest()
+    sha1_hash = hashlib.sha1(open(filename, 'rb').read()).hexdigest()
     return any(page.exists() for page in site.allimages(sha1=sha1_hash))
 
-def upload_to_commons(url: str, filename: str, description: str) -> None:
+def upload_to_commons(filepath: str, filename: str, description: str, edit_summary: str) -> None:
     """Upload the file to Wikimedia Commons using UploadRobot."""
     site = pywikibot.Site('commons', 'commons')
-
-    # See this example for more parameters -
-    # https://doc.wikimedia.org/pywikibot/master/_modules/scripts/upload.html
-    expanded_url = get_final_url(url)
-    upload_bot = UploadRobot(url=expanded_url,
+    upload_bot = UploadRobot([filepath],
                              description=description,
                              use_filename=filename,
                              keep_filename=True,
                              verify_description=False,
+                             summary=edit_summary,
                              target_site=site)
     upload_bot.run()
 
@@ -88,41 +87,21 @@ def process_csv(csv_file: str) -> None:
         for row in reader:
             record_id, url, filename, edit_summary, description = row
             print (f"Processing {record_id}")
-            image_data = download_image(url)
-            if image_data and not file_exists_on_commons(image_data, filename):
-                upload_to_commons(url, filename, description)
+            filepath = download_image(url, filename)
+            if filepath and not file_exists_on_commons(filepath):
+                upload_to_commons(filepath, filename, description, edit_summary)
                 print(f"Uploaded {filename} to Wikimedia Commons.")
             else:
                 print(f"File {filename} already exists on Wikimedia Commons or download failed.")
-
-def process_csv_data(csv_data: List[List[str]]) -> None:
-    """Process each row of the CSV data and upload images."""
-    for row in csv_data:
-        # record_id, source_image_url, commons_filename, edit_summary, description
-        record_id, url, filename, edit_summary, description = row
-        print (f"Processing {record_id}")
-        image_data = download_image(url)
-        if image_data and not file_exists_on_commons(image_data, filename):
-            upload_to_commons(url, filename, description)
-            print(f"Uploaded {filename} to Wikimedia Commons.")
-        else:
-            print(f"File {filename} already exists on Wikimedia Commons or download failed.")
-
-def read_csv_from_stdin() -> List[List[str]]:
-    """Read CSV formatted data from stdin and return it as a list of lists."""
-    reader = csv.reader(sys.stdin)
-    return list(reader)
+            if filepath and os.path.exists(filepath):
+                os.remove(filepath)
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="Upload images to Wikimedia Commons from a CSV file or stdin.")
-    parser.add_argument("csv_file", nargs='?', help="Path to the CSV file containing the image URLs, filenames, and descriptions. If omitted, reads from stdin.")
+    parser = argparse.ArgumentParser(description="Upload images to Wikimedia Commons from a CSV file.")
+    parser.add_argument("csv_file", help="Path to the CSV file containing the image URLs, filenames, and descriptions.")
     args = parser.parse_args()
 
-    if args.csv_file:
-        process_csv(args.csv_file)
-    else:
-        csv_data = read_csv_from_stdin()
-        process_csv_data(csv_data)
+    process_csv(args.csv_file)
 
 if __name__ == "__main__":
     main()
